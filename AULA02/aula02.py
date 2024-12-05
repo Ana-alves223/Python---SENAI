e9e026bb1ed75cfec1c611be6ce0c73372615076
Ana-alves223 <132944669+Ana-alves223@users.noreply.github.com>
2024-12-05 11:26:33 -0300

    Add files via upload
    Aula 2 - variáveis

---
+++ AULA02/aula02.py
@@ -0,0 +1,20 @@
+print("Bom dia!")
+
+nome_usuario = "Ana Claudia"    # str = string
+
+idade_usuario = 20    # int = interger/inteiro
+
+altura_usuario = 1.55  # float = flutuante
+
+maior_idade = True # boo = boolean
+
+print("Seja bem-vindo", nome_usuario)
+print("Sua idade é", idade_usuario, "anos")
+print("Você tem", altura_usuario, "metros")
+print("Você é maior de idade?", maior_idade)
+
+
+
+
+
+
